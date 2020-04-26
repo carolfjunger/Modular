2159d38775bbd084100682432d2ea743a17155e0
Ana Carolina Junger <carolfjunger@gmail.com>
2020-04-26 20:01:47 -0300

    funcoes de acesso criadas para o modulo cartela

---
+++ Cartela.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+'''
+    Nome: Cartela
+    Descriçao: Modulo responsavel por gerenciar uma Cartela num jogo de Yathzee
+    Funcoes de Acesso: cria, somaPontuacao, validaPosicao
+    Autores: ACJ - Ana Carolina Junger
+    Historico de evolucao:
+        Autor         Versao            Data              Observacao
+        ACJ             1               26/04/2020          criacao das primeiras funcoes
+        
+'''
+
+import Jogador
+__all__ = ["cria", "somaPontuacao"]
+
+
+cartelas = []
+
+
+
+'''
+    Definição:
+        Função responsável por criar uma cartela em uma partida de Yahtzee.
+    Parâmetros:
+        nome: String com o nome do jogador
+    Retorno: 
+        1: caso a cartela tenha sido criado com sucesso
+        -1: caso o jogadorId nao seja um numero
+        -2: caso nao exista um jogador com esse id
+        -3: caso ja exista uma cartela para esse jogador
+'''
+def cria(jogadorId):
+    if (type(jogadorId) != int):
+        return -1
+    jogadores = Jogador.jogadores
+    idExiste = False
+    for jogador in jogadores:
+        if ( jogador["id"] == jogadorId):
+            idExiste = True
+            break
+    if (idExiste != True):
+        return -2
+    jogadorJaTemCartela = False
+    for cartela in cartelas:
+        if( cartela["jogadorId"] == jogadorId):
+            jogadorJaTemCartela = True
+            break
+    if (jogadorJaTemCartela):
+        return -3
+    index = len(cartelas) - 1
+    if (index > 0):
+        lastId = cartelas[len(cartelas) - 1]["id"]
+    else:
+        lastId = -1
+    cartela = { "id": lastId + 1 ,"jogadorId": jogadorId, "pontuacoes": {"jogadaDeUm": -1, "jogadaDeDois": -1, "jogadaDeTres": -1, "jogadaDeQuatro": -1, "jogadaDeCinco": -1, "jogadaDeSeis": -1, "trinca": -1, "quadra": -1, "fullHouse": -1, "sequenciaAlta": -1, "sequenciaBaixa": -1, "general": -1, "jogadaAleatoria": -1} }
+    cartelas.append(cartela)
+    return 1
+
+
+'''
+    Definição:
+        Função responsável limpar a lista de cartelas.
+    Parâmetros:
+    
+    Retorno: 
+    
+'''
+def limpa_cartelas():
+    while(cartelas != []): 
+        cartelas.pop()
+
+'''
+    Definição:
+        Função responsável por verificar se uma cartela existe.
+    Parâmetros:
+        jogadorId: Id do jogador
+    Retorno: 
+        i: index da cartela caso ela exista
+        -1: caso a cartela nao exista
+
+'''
+def verificaSeACartelaExiste (jogadorId):
+    for i in range(0, len(cartelas)):
+        if(cartelas[i]["jogadorId"] == jogadorId):
+            return i
+    return -1
+
+
+'''
+    Definição:
+        Função responsável por preencher uma cartela em uma partida de Yahtzee.
+    Parâmetros:
+        jogadorId: Id do jogador
+        posicaoCartela: nome da posicao a ser preenchida na cartela
+        ponto: valor dos pontos a serem preenchidos
+    Retorno: 
+        1: caso a cartela tenha sido preenchida com sucesso
+        -1: caso o jogadorId nao seja um numero
+        -2: caso o jogador nao tenha cartela
+        -3: caso a posicao da cartela nao seja uma string
+        -4: caso o nome da pontuacao nao exista
+        -5: caso os pontos nao sejam um numero inteiro
+        -6: caso os pontos sejam menores que zero
+'''
+
+def preenche(jogadorId,posicaoCartela, pontos):
+    if (type(jogadorId) != int):
+        return -1
+    index = verificaSeACartelaExiste(jogadorId)
+    if (index == -1):
+        return -2
+    if (type(posicaoCartela) != str):
+        return -3
+    cartela = cartelas[index]
+    pontuacoes = cartela["pontuacoes"]
+    pontuacaoExiste = False
+    for nomeDaPontuacao in pontuacoes:
+        if (nomeDaPontuacao == posicaoCartela):
+            pontuacaoExiste = True
+            break
+    if (pontuacaoExiste == False):
+        return -4
+    if (type(pontos) != int):
+        return -5
+    if (pontos < 0):
+        return -6
+    pontuacoes[posicaoCartela] = pontos 
+    return 1
+
+
+'''
+    Definição:
+        Função responsável por retornar a pontuacao final de um jogador em uma partida de Yahtzee.
+    Parâmetros:
+        jogadorId: Id do jogador
+    Retorno: 
+        1: caso a soma tenha sido realizada com sucesso
+        -1: caso o jogadorId nao seja um numero
+        -2: caso o jogador nao tenha cartela
+        -3: caso ocorra erro na funcao vinculaPontuacaoFinalAoJogador
+'''
+def somaPontuacao (jogadorId):
+    if (type(jogadorId) != int):
+        return -1
+    index = verificaSeACartelaExiste(jogadorId)
+    if (index == -1):
+        return -2
+    pontuacoes = cartelas[index]["pontuacoes"]
+    totalDePontos = 0
+    for pontuacao in pontuacoes.values():
+        totalDePontos += pontuacao
+    retorno = Jogador.vinculaPontuacaoFinalAoJogador(jogadorId, totalDePontos)
+    if(retorno == 1):
+        return 1
+    return -3
--- Teste.py
+++ Teste.py
@@ -3,6 +3,7 @@ import unittest
 import Jogador
 import Partida
 import Rodada
+import Cartela
 
 class VerificarPangramaTests(unittest.TestCase):
     def testa_01_funcao_cria_modulo_jogador(self):
@@ -25,6 +26,33 @@ class VerificarPangramaTests(unittest.TestCase):
         print("Caso de Teste 04  Funcao Cria Jogador- Funcao retorna 1 caso o jogador tem sido criado ")
         retorno_esperado = Jogador.cria("Pedro")
         self.assertEqual(retorno_esperado, 1)
+    
+    def testa_01_funcao_vinculaPontuacaoFinalAoJogador_modulo_jogador(self):
+        print("Caso de Teste 01 Funcao vinculaPontuacaoFinalAoJogador - Funcao retorna -1 caso o jogador Id nao exista")
+        Jogador.limpa_jogadores()
+        retorno_esperado = Jogador.vinculaPontuacaoFinalAoJogador(0, 25)
+        self.assertEqual(retorno_esperado, -1)
+
+    def testa_02_funcao_vinculaPontuacaoFinalAoJogador_modulo_jogador(self):
+        print("Caso de Teste 02 Funcao vinculaPontuacaoFinalAoJogador - Funcao retorna -2 caso o jogador Id nao seja um id")
+        retorno_esperado = Jogador.vinculaPontuacaoFinalAoJogador("oi", 25)
+        self.assertEqual(retorno_esperado, -2)
+
+    def testa_03_funcao_vinculaPontuacaoFinalAoJogador_modulo_jogador(self):
+        print("Caso de Teste 03 Funcao vinculaPontuacaoFinalAoJogador - Funcao retorna -3 caso a total de pontos nao seja um int")
+        Jogador.limpa_jogadores()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        retorno_esperado = Jogador.vinculaPontuacaoFinalAoJogador(jogadorId, "oi")
+        self.assertEqual(retorno_esperado, -3)
+
+    def testa_04_funcao_vinculaPontuacaoFinalAoJogador_modulo_jogador(self):
+        print("Caso de Teste 04 Funcao vinculaPontuacaoFinalAoJogador - Funcao retorna -4 caso a total de pontos seja menor que 0")
+        Jogador.limpa_jogadores()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        retorno_esperado = Jogador.vinculaPontuacaoFinalAoJogador(jogadorId, -1)
+        self.assertEqual(retorno_esperado, -4)
     
     def teste_01_funcao_cria_modulo_partida(self):
         print("Caso de Teste 01 Funcao Cria Partida- Funcao retorna -1 caso array de jogadores tenha tamanho menor que 2")
@@ -141,3 +169,140 @@ class VerificarPangramaTests(unittest.TestCase):
         retorno_esperado = Rodada.finaliza()
         self.assertEqual(retorno_esperado, 1)
 
+    def teste_01_funcao_cria_modulo_cartela(self):
+        print("Caso de Teste 01 Funcao Cria Cartela- Funcao retorna -1 caso nao receba um Id de jogador como parametro")
+        retorno_esperado = Cartela.cria("")
+        self.assertEqual(retorno_esperado, -1)
+
+    def teste_02_funcao_cria_modulo_cartela(self):
+        print("Caso de Teste 02 Funcao Cria Cartela- Funcao retorna -2 caso nao o Id nao seja vinculado a nenhum jogador existente")
+        Jogador.limpa_jogadores()
+        Jogador.cria("Joao")
+        retorno_esperado = Cartela.cria(50)
+        self.assertEqual(retorno_esperado, -2)
+
+    def teste_03_funcao_cria_modulo_cartela(self):
+        print("Caso de Teste 03 Funcao Cria Cartela- Funcao retorna -3 caso o jogador exista e ja possua uma cartela")
+        Jogador.limpa_jogadores()
+        Jogador.cria("Joao")
+        Cartela.cria(0)
+        retorno_esperado = Cartela.cria(0)
+        self.assertEqual(retorno_esperado, -3)
+
+    def teste_04_funcao_cria_modulo_cartela(self):
+        print("Caso de Teste 04 Funcao Cria Cartela- Funcao retorna 1 caso o jogador exista e ja nao possua uma cartela")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        retorno_esperado = Cartela.cria(0)
+        self.assertEqual(retorno_esperado, 1)
+
+    def teste_01_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 01 Funcao Preenche Cartela- Funcao retorna -1 caso nao receba um Id de jogador como parametro")
+        retorno_esperado = Cartela.preenche("", "somaDeUm", 1)
+        self.assertEqual(retorno_esperado, -1)
+
+    def teste_02_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 02 Funcao Preenche Cartela- Funcao retorna -2 caso o Id de jogador nao esteja vinculado a nenhuma cartela")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        retorno_esperado = Cartela.preenche(50,"somaDeUm", 1)
+        self.assertEqual(retorno_esperado, -2)
+
+    def teste_03_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 03 Funcao Preenche Cartela- Funcao retorna -3 caso a posicao na cartela nao seja uma string")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.preenche(jogadorId, 0, 1)
+        self.assertEqual(retorno_esperado, -3)
+
+    def teste_04_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 04 Funcao Preenche Cartela- Funcao retorna -4 caso a posicao na cartela nao exista")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.preenche(jogadorId, "somaDeSete", 1)
+        self.assertEqual(retorno_esperado, -4)
+
+    def teste_05_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 05 Funcao Preenche Cartela- Funcao retorna -5 caso os pontos nao sejam um inteiro")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.preenche(jogadorId, "jogadaDeUm", "oi")
+        self.assertEqual(retorno_esperado, -5)
+
+    def teste_06_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 06 Funcao Preenche Cartela- Funcao retorna -6 caso os pontos sejam um inteiro menor que zero")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.preenche(jogadorId, "jogadaDeUm", -1)
+        self.assertEqual(retorno_esperado, -6)
+
+    def teste_07_funcao_preenche_modulo_cartela(self):
+        print("Caso de Teste 07 Funcao Preenche Cartela- Funcao retorna 1 caso a pontuacao tenha sido preenchidaa com sucesso")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.preenche(jogadorId, "jogadaDeUm", 1)
+        self.assertEqual(retorno_esperado, 1)
+
+    def teste_01_funcao_somaPontuacao_modulo_cartela(self):
+        print("Caso de Teste 01 Funcao Soma Pontuacao Cartela- Funcao retorna -1 caso nao receba um Id de jogador como parametro")
+        retorno_esperado = Cartela.somaPontuacao("")
+        self.assertEqual(retorno_esperado, -1)
+
+    def teste_02_funcao_somaPontuacao_modulo_cartela(self):
+        print("Caso de Teste 02 Funcao Soma Pontuacao Cartela- Funcao retorna -2 caso o Id de jogador nao esteja vinculado a nenhuma cartela")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.somaPontuacao(50)
+        self.assertEqual(retorno_esperado, -2)
+
+    def teste_03_funcao_somaPontuacao_modulo_cartela(self):
+        print("Caso de Teste 03 Funcao Soma Pontuacao Cartela- Funcao retorna -3 caso de algum erro na funcao vinculaPontuacao")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        retorno_esperado = Cartela.somaPontuacao(jogadorId)
+        self.assertEqual(retorno_esperado, -3)
+    
+    def teste_04_funcao_somaPontuacao_modulo_cartela(self):
+        print("Caso de Teste 04 Funcao Soma Pontuacao Cartela- Funcao retorna 1 caso pontuacao tenha sido somada com sucesso")
+        Jogador.limpa_jogadores()
+        Cartela.limpa_cartelas()
+        Jogador.cria("Joao")
+        jogadorId = Jogador.pegaJogadorId("Joao")
+        Cartela.cria(jogadorId)
+        Cartela.preenche(jogadorId, "jogadaDeUm", 1)
+        Cartela.preenche(jogadorId, "jogadaDeDois", 2)
+        Cartela.preenche(jogadorId, "jogadaDeTres", 3)
+        Cartela.preenche(jogadorId, "jogadaDeQuatro", 4)
+        Cartela.preenche(jogadorId, "jogadaDeCinco", 5)
+        Cartela.preenche(jogadorId, "jogadaDeSeis", 6)
+        Cartela.preenche(jogadorId, "trinca", 3)
+        Cartela.preenche(jogadorId, "quadra", 4)
+        Cartela.preenche(jogadorId, "fullHouse", 25)
+        Cartela.preenche(jogadorId, "sequenciaAlta", 30)
+        Cartela.preenche(jogadorId, "sequenciaBaixa", 40)
+        Cartela.preenche(jogadorId, "general", 50)
+        Cartela.preenche(jogadorId, "jogadaAleatoria", 30)
+        retorno_esperado = Cartela.somaPontuacao(jogadorId)
+        self.assertEqual(retorno_esperado, 1)
